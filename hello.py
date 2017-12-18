78ea2d507967e424d2aba29d7b87b7f1a13bce1e
teatran <teaprogramming@gmail.com>
2017-12-18 19:51:56 +0700

    My first commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
